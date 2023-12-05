4543f58551b3dcefaddc41f218848b29e85d8bea
eunjeong Kim <guri2913@gmail.com>
2023-12-05 18:00:28 +0900

    댓글 기능

--- src/main/java/com/example/Reply/Entity/Reply.java
+++ src/main/java/com/example/Reply/Entity/Reply.java
@@ -1,26 +1,26 @@
-//package com.example.Reply.Entity;
-//
-//import com.example.Board.Entity.Board;
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//
-//@Entity
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@ToString(exclude = "board")
-//public class Reply extends BaseEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long rno;
-//
-//    private String text;
-//
-//    private String replier;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Board board;
-//}
+package com.example.Reply.Entity;
+
+import com.example.Board.Entity.Board;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "board")
+public class Reply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    private String text;
+
+    private String replier;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;
+}
--- src/main/java/com/example/Reply/Service/ReplyService.java
+++ src/main/java/com/example/Reply/Service/ReplyService.java
@@ -1,44 +1,45 @@
-//package com.example.Reply.Service;
-//
-//import com.example.Board.Entity.Board;
-//import com.example.Reply.Dto.ReplyDTO;
-//import com.example.Reply.Entity.Reply;
-//import com.example.Trip.Entity.Trip;
-//
-//import java.util.List;
-//
-//public interface ReplyService {
-//
-//    Long register(ReplyDTO replyDTO);
-//
-//    List<ReplyDTO> getList(Long id);
-//
-//    void modify(ReplyDTO replyDTO);
-//
-//    void remove(Long rno);
-//
-//    default Reply dtoToEntity(ReplyDTO replyDTO) {
-//        Board board = Board.builder().bno(replyDTO.Bno()).build();
-//
-//        Reply reply = Reply.builder()
-//                .rno(replyDTO.getRno())
-//                .text(replyDTO.getText())
-//                .replier(replyDTO.getReplier())
-//                .board(board)
-//                .build();
-//
-//        return reply;
-//    }
-//
-//    default ReplyDTO entityToDTO(Reply reply) {
-//        ReplyDTO dto = ReplyDTO.builder()
-//                .rno(reply.getRno())
-//                .text(reply.getText())
-//                .replier(reply.getReplier())
-//                .regDate(reply.getRegDate())
-//                .modDate(reply.getModDate())
-//                .build();
-//
-//        return dto;
-//    }
-//}
+package com.example.Reply.Service;
+
+import com.example.Board.Entity.Board;
+import com.example.Reply.Dto.ReplyDTO;
+import com.example.Reply.Entity.Reply;
+import com.example.Trip.Entity.Trip;
+
+import java.util.List;
+
+public interface ReplyService {
+
+    Long register(ReplyDTO replyDTO);  //댓글 등록
+
+    List<ReplyDTO> getList(Long bno);       //특정 게시물의 댓글 목록
+
+    void modify(ReplyDTO replyDTO); //댓글 수정
+
+    void remove(Long rno);  //댓글 삭제
+
+    default Reply dtoToEntity(ReplyDTO replyDTO) {
+        Board board = Board.builder().bno(replyDTO.getBno()).build();
+
+        Reply reply = Reply.builder()
+                .rno(replyDTO.getRno())
+                .text(replyDTO.getText())
+                .replier(replyDTO.getReplier())
+                .board(board)
+                .build();
+
+        return reply;
+    }
+
+    //Reply객체를 ReplyDto로 변환, Board 객체가 필요하지 않으므로 게시물 번호만
+    default ReplyDTO entityToDTO(Reply reply) {
+        ReplyDTO dto = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .text(reply.getText())
+                .replier(reply.getReplier())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+
+        return dto;
+    }
+}
