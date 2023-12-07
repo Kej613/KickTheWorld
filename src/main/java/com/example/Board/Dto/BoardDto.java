8804b4ac9531cb9d7d4e3b68f0fbc10115dd3465
eunjeong Kim <guri2913@gmail.com>
2023-12-07 12:17:02 +0900

    게시판페이지, 댓글 수정

--- src/main/java/com/example/Board/Dto/BoardDto.java
+++ src/main/java/com/example/Board/Dto/BoardDto.java
@@ -13,6 +13,7 @@ public class BoardDto {
     private Long bno;
     private String title;
     private String content;
+    private String category; //카테고리
     private String writer;      //작성자
     private LocalDateTime regDate, modDate;
     private int replyCount; //댓글 수
