e1e905b4eb15112e4ce6cdecf54244ff2986db9e
euiyeon0519 <euiyeon0519@gmail.com>
2023-12-07 15:35:08 +0900

    경로 수정

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 //
     @GetMapping("/admin")
     public String adminPage() {
-        return "adminPage";
+        return "adminPage_test";
     }
 
 //    @GetMapping("/")
