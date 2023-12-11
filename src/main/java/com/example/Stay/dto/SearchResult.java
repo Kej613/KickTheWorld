b8c94e7ff5a5f8f743ad6f65fb7acfd14120b8f6
eunjeong Kim <guri2913@gmail.com>
2023-12-11 14:28:51 +0900

    기타 파일처리

--- src/main/java/com/example/Stay/dto/SearchResult.java
+++ src/main/java/com/example/Stay/dto/SearchResult.java
@@ -1,23 +1,30 @@
 package com.example.Stay.dto;
 
+import com.example.Eatery.Dto.EateryItemDto;
+import com.example.Eatery.Entity.Eatery;
 import com.example.Stay.Entity.Stay;
+import com.example.Trip.Dto.MainItemDto;
 import com.example.Trip.Entity.Trip;
 import org.springframework.data.domain.Page;
 
 public class SearchResult {
-    private final Page<Stay> stays;
-    private final Page<Trip> trips;
+    private final Page<StayItemDto> stays;
+    private final Page<MainItemDto> trips;
+    private final Page<EateryItemDto> eaterys;
 
-    public SearchResult(Page<Stay> stays, Page<Trip> trips) {
+    public SearchResult(Page<StayItemDto> stays, Page<MainItemDto> trips, Page<EateryItemDto> eaterys) {
         this.stays = stays;
         this.trips = trips;
+        this.eaterys = eaterys;
     }
 
-    public Page<Stay> getStays() {
+    public Page<StayItemDto> getStays() {
         return stays;
     }
 
-    public Page<Trip> getTrips() {
+    public Page<MainItemDto> getTrips() {
         return trips;
     }
+
+    public Page<EateryItemDto> getEaterys() { return eaterys; }
 }
