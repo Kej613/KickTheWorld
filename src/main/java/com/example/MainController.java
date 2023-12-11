e865262dc9a95173ce2b610e221a05ee78331c9d
eunjeong Kim <guri2913@gmail.com>
2023-12-11 14:28:05 +0900

    로그인/회원가입 security 수정

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -1,20 +1,37 @@
 package com.example;
 
+import com.example.Eatery.Dto.EateryItemDto;
+import com.example.Eatery.Entity.Eatery;
+import com.example.Eatery.Service.EateryService;
+import com.example.Stay.Entity.Stay;
+import com.example.Stay.Service.StayService;
+import com.example.Stay.dto.SearchResult;
+import com.example.Stay.dto.StayItemDto;
+import com.example.Trip.Dto.MainItemDto;
+import com.example.Trip.Entity.Trip;
+import com.example.Trip.Service.TripService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
 @Controller
 public class MainController {
-//
+
+    private final TripService tripService;
+    private final StayService stayService;
+    private final EateryService eateryService;
+
     @GetMapping("/admin")
     public String adminPage() {
         return "adminPage";
     }
 
-//    @GetMapping("/")
-//    public String mainPage() {
-//        return "mainPage";
-//    }
 
     @GetMapping("/")
     public String mainPage() {
@@ -25,6 +42,31 @@ public class MainController {
     @GetMapping("/mypage")
     public String myPage() {
         return "myPage";
+    }
+
+
+    @GetMapping("/kakao")
+    public String kakaoPage() { return "kakaomap"; }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(value = "address", required = false) String address,
+                         @RequestParam(value = "category", required = false) String category,
+                         @RequestParam(value = "theme", required = false) String theme,
+                         @RequestParam(value = "eaterycategory", required = false) String eaterycategory,
+                         @PageableDefault(page = 0, size = 10) Pageable pageable,
+                         Model model) {
+        Page<StayItemDto> stays = stayService.findByCategory(category, address, pageable);
+        Page<MainItemDto> trips = tripService.findByCategory(theme, address, pageable);
+        Page<EateryItemDto> eaterys = eateryService.findByAddressAndEateryCategory(eaterycategory, address, pageable);
+
+        model.addAttribute("stays", stays);
+        model.addAttribute("trips", trips);
+        model.addAttribute("eaterys", eaterys);
+
+        SearchResult result = new SearchResult(stays, trips, eaterys);
+        model.addAttribute("result", result);
+
+        return "recommendPage";
     }
 
 }
--- src/main/java/com/example/Member/config/WebConfig.java
+++ src/main/java/com/example/Member/config/WebConfig.java
@@ -1,21 +1,21 @@
-package com.example.Member.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// 전역 설정
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("/members/login")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-}
+//package com.example.Member.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//// 전역 설정
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("/members/login")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("Authorization", "Content-Type")
+//                .exposedHeaders("Custom-Header")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
+//}
